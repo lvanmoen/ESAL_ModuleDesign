45226001de5592fb14e3d4235bcf505913b2df06
lvanmoen <31223874+lvanmoen@users.noreply.github.com>
2017-09-13 18:04:59 -0400

    updated h

--- module.h
+++ module.h
@@ -7,6 +7,7 @@
 
 //group3
 
+
 //group4
 
 //group5
@@ -56,6 +57,16 @@
 
 //group3
 
+// Use an enum for port names to make it more friendly to the service layer.
+typedef enum {
+	PORTA = 0, 
+	PORTB = 1, 
+	PORTC = 2, 
+	PORTD = 3, 
+	PORTE = 4, 
+	PORTF = 5
+} PortName_t;
+
 //group4
 
 //group5
@@ -86,6 +97,8 @@
 //group2
 
 //group3
+void SetupPins(PortName_t port, uint8_t pinNumber);
+void SendLatch(void);
 
 //group4
 
